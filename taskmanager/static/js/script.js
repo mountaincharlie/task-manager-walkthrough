731e6d8a332e9a714477b92527fe8ac2a75f8344
Charlie <ciharland@hotmail.co.uk>
2022-03-22 12:05:47 +0000

    Create custom css and js files to link into base.html

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // initialisation snippet for Materialize's sidenav (with custom var names)
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });

11e662781c941ab9cefdba51dc69851bd2b24c06
Charlie <ciharland@hotmail.co.uk>
2022-03-24 17:30:51 +0000

    Add materialize's initialisation for datepicker and select

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,4 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // initialisation snippet for Materialize's sidenav (with custom var names)
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialisation (see materialize datepicker doc)
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format:"dd mmmm, yyyy",
+      i18n: {done: "select"},
+    });
+
+    // select initialisation (see materialize datepicker doc)
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });

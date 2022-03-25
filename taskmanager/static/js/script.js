86b834f65ea91391b596f581ad8194edd029676f
Charlie <ciharland@hotmail.co.uk>
2022-03-25 11:26:58 +0000

    Add initialization code for collapsible

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,4 @@
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
     // initialisation snippet for Materialize's sidenav (with custom var names)
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
@@ -11,7 +10,11 @@ document.addEventListener('DOMContentLoaded', function() {
       i18n: {done: "select"},
     });
 
-    // select initialisation (see materialize datepicker doc)
+    // select initialisation (see materialize select doc)
     var selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
-  });
+
+    // collabsilbe initialisation (see materialize collabsilbe doc)
+    var collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
+});
